1bdef5d228b57af3dc099a7cca0063eb76222236
shane <shane@gccompany.co.kr>
2022-08-11 17:02:29 +0900

    create LottieTest Component

---
+++ examples/vue3/src/test/LottieTest.story.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import lottie from "lottie-web";
+import { onMounted, ref } from "vue";
+import lottieData from "../lottie-data";
+
+const el = ref(null as null | HTMLElement);
+
+onMounted(() => {
+  const animData = {
+    container: el.value as HTMLElement,
+    animType: "svg",
+    loop: true,
+    prerender: true,
+    autoplay: true,
+    data: lottieData,
+  };
+  lottie.loadAnimation(animData);
+});
+</script>
+
+<template>
+  <Story title="Lottie Test">
+    <Variant title="default">
+      Lottie Test!
+      <div ref="el"></div>
+    </Variant>
+  </Story>
+</template>
